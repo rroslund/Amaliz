e47a985cfef59756eb76e182013cc098628ea364
unknown <rroslund@p-dv-dsk-rros.kcura.corp>
2013-05-10 11:20:26 -0500

    Fixing javascript defect by using the server to call the PS2 api.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, urllib
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -6,6 +6,15 @@ app = Flask(__name__)
 app.config.update(
         DEBUG = True,
         )
+
+
+def getBRank(name):
+    url =
+    'http://census.soe.com/get/ps2-beta/charater/?name.first_lower='+name+'&c:show=experience.rank'
+    return urllib.urlopen(url).read()
+
+
+
 
 #----
 # controllerz
@@ -21,6 +30,13 @@ def index():
 @app.route("/test")
 def test():
     return "This is a test"
+
+@app.route("/brank/<username>")
+def brank(username):
+    return getBRank(username)
+
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
