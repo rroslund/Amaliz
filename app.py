eaf488e898e17aab382c1ef2bb9ecd9be90d7f3b
Rich Roslund <rroslund890@gmail.com>
2013-05-12 23:13:10 -0500

    Temp commit containing test urls. The weapon_death graph should be functional...

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, urllib
+import os, urllib, json, sys
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -7,10 +7,54 @@ app.config.update(
         DEBUG = True,
         )
 
+baseurl = 'http://census.soe.com/get/ps2-beta/'
+def ggetCharId(name):
+    url='htrp://census.soe.com/get/ps2-beta/character/?name.first_lower='+name+'&c:show=name.first'
+    return int(json.loads(urllib.urlopen(url).read())['character_list'][0]['id'])
 
-def getBRank(name):
+def ggetBRank(name):
     url = 'http://census.soe.com/get/ps2-beta/character/?name.first_lower='+name+'&c:show=experience.rank&c:show=name.first'
     return urllib.urlopen(url).read()
+
+def getCharId(name):
+    return 9001
+
+def getItemTESTJson(url):
+    f = file('/Users/rich/Documents/items.json','r')
+    res =  json.loads(f.read())
+    f.close()
+    return res
+def getJson(url):
+    f = file('/Users/rich/Documents/amalizDeaths.json','r')
+    res =  json.loads(f.read())
+    f.close()
+    return res
+
+
+def ggetJson(url):
+    return json.loads(urllib.urlopen(url).read())
+
+def getDeathsByWeapon(name,numToCheck):
+    charId = getCharId(name);
+    url = baseurl+'/characters_event/'+str(charId)+'?type=DEATH&c:limit='+str(numToCheck)
+    res = getJson(url)
+    weaponInfo = [x['attacker_weapon_id'] for x in res['characters_event_list'] if int(x['attacker_weapon_id'])!=0]
+    uniqueWeps = set(weaponInfo)
+    wepDeathCount = {x:weaponInfo.count(x) for x in uniqueWeps}
+    wepRank = list(set(wepDeathCount.values()))
+    wepRank.sort()
+    wepRes = [x for x in wepDeathCount.items() if x[1] > wepRank[len(wepRank)-10]]
+    return json.dumps([(getItemName(x[0]),x[1]) for x in wepRes])
+
+def getItemName(itemId):
+    url = baseurl+'/item/'+str(itemId)+'?c:show=name'
+    jsonres = getItemTESTJson(url)
+    res = [x['name']['en'] for x in jsonres['item_list'] if int(x['id'])==int(itemId)]
+    if res != None and len(res)>0:
+        return res[0]
+    else:
+        return 'UNKNOWN'
+            
 
 
 
@@ -33,6 +77,10 @@ def test():
 @app.route("/brank/<username>")
 def brank(username):
     return getBRank(username)
+@app.route("/weapon_deaths/<userOne>&<amount>")
+def deathsByWep(userOne,amount):
+    return getDeathsByWeapon(userOne,amount)
+
 
 
 
@@ -40,5 +88,4 @@ def brank(username):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host='0.0.0.0', port=port)
-
 
---
+++ static/js/amaliz.js
@@ -0,0 +1,42 @@
+function makeGraph(data,divId){
+   return $.plot(divId, [ data ], {
+      grid: {
+      hoverable: true
+      },
+      series: {
+bars: {
+show: true,
+barWidth: 0.6,
+align: "center"
+}
+},
+xaxis: {
+mode: "categories",
+tickLength: 0
+}
+});
+}
+function addToGraph(plot,data){
+  plot.setData([data]);
+  plot.setupGrid();
+  plot.draw();
+}
+ 
+
+function makeWeaponGraph(){
+ var wplot = makeGraph([[]],"#wepgraph");
+$.ajax({
+type: "GET", 
+async: true,
+url:"/weapon_deaths/amaliz&1000",
+dataType:"json",
+success: 
+function(data){
+  var gData = data;
+  addToGraph(wplot,gData);
+  },
+error: function(request, errorText, errorCode){alert(errorText);} });
+
+
+
+}
